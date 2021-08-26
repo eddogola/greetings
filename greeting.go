cd40edce5675b7c6697cfc5dd39871c4378422c5
Eddy Ogola <eddy.ogola1@gmail.com>
2021-08-26 01:40:11 -0400

    Greet multiple people

--- greeting.go
+++ greeting.go
@@ -2,6 +2,7 @@ package greetings
 
 import (
 	"fmt"
+	"math/rand"
 	"errors"
 )
 
@@ -9,5 +10,27 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty string")
 	}
-	return fmt.Sprintf("Hii, %v. Welcome!", name), nil
+	return fmt.Sprintf(greetingFormat(), name), nil
+}
+
+func Hellos(name string) (map[string]string, error) {
+	if name == "" {
+		return nil, errors.New("no name provided")
+	}
+
+	return map[string]string{
+		name: fmt.Sprintf(greetingFormat(), name),
+	}, nil
+}
+
+func greetingFormat() string {
+	greetings := []string{
+		"Hello, %v",
+		"Nice to see you %v",
+		"Most Welcome %v",
+	}
+
+	greeting := greetings[rand.Intn(len(greetings))]
+
+	return greeting
 }
